2ed7df949b46cf7e797261761da69429e4a604a6
Roc Boronat <roc@fewlaps.com>
2016-04-06 00:38:59 +0200

    Adding clarifications to IntroducingTheLibraryTest

--- src/test/java/com/fewlaps/quitnowemailsuggester/IntroducingTheLibraryTest.java
+++ src/test/java/com/fewlaps/quitnowemailsuggester/IntroducingTheLibraryTest.java
@@ -18,6 +18,9 @@ public class IntroducingTheLibraryTest {
         String goodEmail = suggester.getSuggestedEmail(badEmail);
         // goodEmail will contain the fixed email address. WOW!
         // And there are tons of email fixes. They're listed at https://goo.gl/IF52EV
+        // In addition, it will never suggest a bad domain. All the suggestions are written one by one,
+        // based on the QuitNow! users and their e-mail bounces. If it doesn't know nothing
+        // better than the input, it will return the same email.
 
         // Well! Something else? Yes: this library hosts some more email related things
         EmailValidator validator = new EmailValidator();
@@ -26,10 +29,10 @@ public class IntroducingTheLibraryTest {
         validator.isAlias(goodEmail); //Is it an alias?
         validator.isDisposable(goodEmail); //Is it listed as a disposable domain?
 
-        // To finish up: if you work with Android, you'll find roc@fewlaps.com:something emails
+        // To finish up: if you work with Android, you'll find roc@fewlaps.com:WhatEVER emails
         // If you want to clean them, here it goes:
         AndroidAccountEmailCleaner cleaner = new AndroidAccountEmailCleaner();
-        String androidAccountEmail = "roc@fewlaps.com:something";
+        String androidAccountEmail = "roc@fewlaps.com:WhatEVER";
         String cleanedEmail = cleaner.cleanEmail(androidAccountEmail);
         // cleanedEmail will contain roc@fewlaps.com
 
