47e195e54af2e7ee4f4a6c9371ed1d93f26256f1
Roc Boronat <roc@fewlaps.com>
2016-04-06 01:04:53 +0200

    Writing the code that creates the File in two lines instead of one, to catch what's crashing the builds at Travis

--- src/main/java/com/fewlaps/quitnowemailsuggester/repository/Repository.java
+++ src/main/java/com/fewlaps/quitnowemailsuggester/repository/Repository.java
@@ -5,71 +5,83 @@ import com.google.gson.Gson;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Repository {
 
-    private final String SUFFIX_FILE_LOCATION = "list/public_suffix_list.dat";
-    private final String DISPOSABLES_FILE_LOCATION = "disposables/index.json";
+    private static final String SUFFIX_FILE_LOCATION = "list/public_suffix_list.dat";
+    private static final String DISPOSABLES_FILE_LOCATION = "disposables/index.json";
+
+    private static List<String> cachedTlds;
+    private static String[] cachedDisposables;
 
     public List<String> getTlds() {
-        try {
-            List<String> lines = new ArrayList();
-            FileInputStream inputStream = null;
-            ClassLoader classLoader = getClass().getClassLoader();
-            File file = new File(classLoader.getResource(SUFFIX_FILE_LOCATION).getFile());
-            Scanner sc = null;
+        if (cachedTlds == null) {
             try {
-                inputStream = new FileInputStream(file);
-                sc = new Scanner(inputStream, "UTF-8");
-                while (sc.hasNextLine()) {
-                    lines.add(sc.nextLine());
+                List<String> lines = new ArrayList();
+                FileInputStream inputStream = null;
+                ClassLoader classLoader = getClass().getClassLoader();
+                URL url = classLoader.getResource(SUFFIX_FILE_LOCATION);
+                File file = new File(url.getFile());
+                Scanner sc = null;
+                try {
+                    inputStream = new FileInputStream(file);
+                    sc = new Scanner(inputStream, "UTF-8");
+                    while (sc.hasNextLine()) {
+                        lines.add(sc.nextLine());
+                    }
+                } finally {
+                    if (inputStream != null) {
+                        inputStream.close();
+                    }
+                    if (sc != null) {
+                        sc.close();
+                    }
                 }
-            } finally {
-                if (inputStream != null) {
-                    inputStream.close();
-                }
-                if (sc != null) {
-                    sc.close();
-                }
-            }
 
-            return lines;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
+                cachedTlds = lines;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
         }
+        return cachedTlds;
     }
 
     public String[] getDisposables() {
-        try {
-            StringBuilder sb = new StringBuilder();
-            FileInputStream inputStream = null;
-            ClassLoader classLoader = getClass().getClassLoader();
-            File file = new File(classLoader.getResource(DISPOSABLES_FILE_LOCATION).getFile());
-            Scanner sc = null;
+        if (cachedDisposables == null) {
             try {
-                inputStream = new FileInputStream(file);
-                sc = new Scanner(inputStream, "UTF-8");
-                while (sc.hasNextLine()) {
-                    sb.append(sc.nextLine());
+                StringBuilder sb = new StringBuilder();
+                FileInputStream inputStream = null;
+                ClassLoader classLoader = getClass().getClassLoader();
+                URL url = classLoader.getResource(DISPOSABLES_FILE_LOCATION);
+                File file = new File(url.getFile());
+                Scanner sc = null;
+                try {
+                    inputStream = new FileInputStream(file);
+                    sc = new Scanner(inputStream, "UTF-8");
+                    while (sc.hasNextLine()) {
+                        sb.append(sc.nextLine());
+                    }
+                } finally {
+                    if (inputStream != null) {
+                        inputStream.close();
+                    }
+                    if (sc != null) {
+                        sc.close();
+                    }
                 }
-            } finally {
-                if (inputStream != null) {
-                    inputStream.close();
-                }
-                if (sc != null) {
-                    sc.close();
-                }
-            }
 
-            Gson gson = new Gson();
-            return gson.fromJson(sb.toString(), String[].class);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
+                Gson gson = new Gson();
+                cachedDisposables = gson.fromJson(sb.toString(), String[].class);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
         }
+        return cachedDisposables;
     }
 }
